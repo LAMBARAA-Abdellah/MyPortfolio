5ee95e82f9efbedac421f0cf47a7de5dbdd6afc6
lambaraa abdellah <abdellahlambaraa@gmail.com>
2022-09-14 00:24:28 +0100

    swiitcher

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* typing animation script */
+var typed = new Typed(".typing", {
+    strings: [" ","Web Developer", "Web Designer", "Freelancer", "Programmer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
